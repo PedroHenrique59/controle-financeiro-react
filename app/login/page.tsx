0b35dbaa07713138979e1dfb4981b5406efd3164
Pedro Henrique <pedro.walter@cnj.jus.br>
2024-11-15 15:33:12 -0300

    implementando login com Clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
       <div className="flex flex-col items-center justify-center">
@@ -18,10 +25,12 @@ const LoginPage = () => {
           Projeto de Controle de Finanças utilizando React + Next.js
         </p>
 
-        <Button variant="outline">
-          Fazer login ou criar uma conta
-          <LogInIcon className="mr-2"></LogInIcon>
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2"></LogInIcon>
+            Fazer login ou criar uma conta
+          </Button>
+        </SignInButton>
       </div>
       <div className="relative h-full w-full">
         <Image
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-const Home = () => {
-  return <h1 className="text-center text-red-500">CUZIN</h1>;
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default Home;
